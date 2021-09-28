6b42cbe619d1139e8fd51879a261ac04e6636118
shainasm <smulani2002@gmail.com>
2021-09-28 07:41:13 -0700

    js operators

---
+++ app.js
@@ -0,0 +1,34 @@
+var a=10
+var b=3
+
+console.log( 'a+b= ', a+b)
+console.log( 'a-b=', a-b)
+console.log( 'aXb=', a*b)
+
+var c=11
+var d=10
+var e='1'
+
+console.warn(c++)
+console.warn(++c)
+console.warn(c++)
+
+console.log(b>c)
+console.log(b<a)
+
+console.log(d==a)
+console.log(d===a)
+
+console.log(d + e)
+
+//logical operator && and ||
+
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && true)
+
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
